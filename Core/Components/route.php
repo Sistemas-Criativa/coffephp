a104a809da0ec10ecf75571d4fbd00320d893910
SistemasCriativa <sistemascriativa@outlook.com>
2020-04-30 17:35:56 -0300

    Add function to generate slugs

--- Core/Components/route.php
+++ Core/Components/route.php
@@ -17,7 +17,22 @@ function route($name = '', $fullURL = false,$args = array()){
 	}
 }
 
+/**
+ * Return a public path folder from project
+ */
 function assets(){
 	return route()['root'];
+}
+
+/**
+ * Generate a Slug
+ */
+function slugfy($str) {
+    $search = array('Ș', 'Ț', 'ş', 'ţ', 'Ş', 'Ţ', 'ș', 'ț', 'î', 'â', 'ă', 'Î', 'Â', 'Ă', 'ë', 'Ë', 'á');
+    $replace = array('s', 't', 's', 't', 's', 't', 's', 't', 'i', 'a', 'a', 'i', 'a', 'a', 'e', 'E', 'a');
+    $str = str_ireplace($search, $replace, strtolower(trim($str)));
+    $str = preg_replace('/[^\w\d\-\ ]/', '', $str);
+    $str = str_replace(' ', '-', $str);
+    return preg_replace('/\-{2,}/', '-', $str);
 }
 ?>
